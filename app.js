bc2cbf50aa2cb5c5250999952ec96c1d71addf2d
GGLVXD <85647114+GGLVXD@users.noreply.github.com>
2023-03-29 21:15:25 +0300

    App

---
+++ app.js
@@ -0,0 +1,41 @@
+// Define variables
+const userList = document.getElementById("userList");
+const search = document.getElementById("search");
+const loadBtn = document.getElementById("loadBtn");
+const usersPerPage = 100;
+let currentPage = 1;
+let users = [];
+
+// Fetch data from JSON file
+fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+    users = data;
+    displayUsers(users.slice(0, usersPerPage));
+  });
+
+// Display users on page
+function displayUsers(users) {
+  userList.innerHTML = "";
+  users.forEach(user => {
+    const li = document.createElement("li");
+    li.classList.add("user");
+    li.innerHTML = `<span class="name">${user.name}</span><span class="amount">$${user.amount}</span>`;
+    userList.appendChild(li);
+  });
+}
+
+// Load more users when button is clicked
+loadBtn.addEventListener("click", () => {
+  currentPage++;
+  const startIndex = (currentPage - 1) * usersPerPage;
+  const endIndex = currentPage * usersPerPage;
+  displayUsers(users.slice(startIndex, endIndex));
+});
+
+// Search users as the user types
+search.addEventListener("keyup", () => {
+  const searchValue = search.value.toLowerCase();
+  const filteredUsers = users.filter(user => user.name.toLowerCase().includes(searchValue));
+  displayUsers(filteredUsers);
+});
