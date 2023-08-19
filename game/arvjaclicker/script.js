168e84fbf8d93e7d94ab5426e6a65649af15afac
GGLVXD <85647114+GGLVXD@users.noreply.github.com>
2023-08-19 19:59:06 +0300

    Update script.js

--- game/arvjaclicker/script.js
+++ game/arvjaclicker/script.js
@@ -6,6 +6,9 @@ let upgrades = [
   { name: "Upgrade 3", cps: 200, cost: 1000 }
 ];
 
+let endgameUpgradeCost = 10000000;
+let endgameUpgradeBought = JSON.parse(localStorage.getItem("endgameUpgradeBought")) || false;
+
 // Load data from local storage
 cookieCount = parseInt(localStorage.getItem("cookieCount")) || cookieCount;
 cookiesPerSecond = parseInt(localStorage.getItem("cookiesPerSecond")) || cookiesPerSecond;
@@ -43,11 +46,39 @@ function buyUpgrade(index) {
 function updateCookies() {
   cookieCount += cookiesPerSecond;
   updateCookieCount();
-  document.getElementById("cookiesPerSecond").textContent = cookiesPerSecond; // Update CPS display
+  document.getElementById("cookiesPerSecond").textContent = cookiesPerSecond;
   saveToLocalStorage();
 }
 
-// Function to save data to local storage
+function updateEndgameUpgrade() {
+  const endgameTitle = document.getElementById("endgame-title");
+  const endgameDescription = document.getElementById("endgame-description");
+  const endgameButton = document.getElementById("endgame-button");
+
+  if (!endgameUpgradeBought) {
+    endgameButton.textContent = `pirkt (10000000 arvji)`;
+    endgameTitle.textContent = "Pabeidz speli";
+    endgameDescription.textContent = "Pabeidz speli lai vari iziet beidzot ara.";
+  } else {
+    endgameButton.textContent = "apsties saiti";
+    endgameTitle.textContent = "Spele ir pabeikta!";
+    endgameDescription.textContent = "Tu pabeidzi speli!";
+  }
+}
+
+function handleEndgameUpgrade() {
+  if (!endgameUpgradeBought && cookieCount >= endgameUpgradeCost) {
+    endgameUpgradeBought = true;
+    localStorage.setItem("endgameUpgradeBought", JSON.stringify(endgameUpgradeBought));
+    updateEndgameUpgrade();
+    // Redirect to a different page here
+    window.location.href = "https://gglvxd.eu.org/game/arvjaclicker/finnaly.html"; // Replace with the actual URL
+  } else if (endgameUpgradeBought) {
+    // Redirect to the endgame page if it's already bought
+    window.location.href = "https://gglvxd.eu.org/game/arvjaclicker/finnaly.html"; // Replace with the actual URL
+  }
+}
+
 function saveToLocalStorage() {
   localStorage.setItem("cookieCount", cookieCount);
   localStorage.setItem("cookiesPerSecond", cookiesPerSecond);
@@ -59,7 +90,10 @@ function saveToLocalStorage() {
 // Initial setup
 setInterval(updateCookies, 1000);
 updateCookieCount();
-document.getElementById("cookiesPerSecond").textContent = cookiesPerSecond; // Initialize CPS display
+document.getElementById("cookiesPerSecond").textContent = cookiesPerSecond;
 for (let i = 0; i < upgrades.length; i++) {
   updateUpgradeCost(i);
 }
+updateEndgameUpgrade();
+document.getElementById("endgame-button").addEventListener("click", handleEndgameUpgrade);
+
