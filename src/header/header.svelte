cdc56844a086b756387b5e2607f57f979b1ce59f
GGLVXD <gglvmax@gmail.com>
2024-10-17 22:34:08 +0300

    added half working header

---
+++ src/header/header.svelte
@@ -0,0 +1,35 @@
+<script>
+	let isMenuOpen = false;
+	let links = [
+		{ name: "Home", url: "/" },
+		{ name: "Status", url: "https://status.gglvxd.eu.org" },
+		{ name: "Repo", url: "https://github.com/gglvxd/website" }
+	];
+
+	const toggleMenu = () => {
+		isMenuOpen = !isMenuOpen;
+	};
+</script>
+
+<header>
+	<div class="logo">
+		<a href="/">GGLVXD.EU.ORG</a>
+	</div>
+
+	<nav>
+		<ul class:isOpen={isMenuOpen} class:isClosed={!isMenuOpen}>
+			{#each links as link}
+				<li><a href={link.url} target="_blank" rel="noopener">{link.name}</a></li>
+			{/each}
+		</ul>
+		<div class="hamburger" on:click={toggleMenu} class:isOpen={isMenuOpen}>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</nav>
+</header>
+
+<style>
+	@import 'header.css';
+</style>
