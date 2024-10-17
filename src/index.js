c586f62a3312fcacebb5a15dab13f49671196254
GGLVXD <gglvmax@gmail.com>
2024-10-17 19:34:35 +0300

    actul website changes i did

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './main/main.jsx';
-import Header from './header/header.jsx';
-import Footer from './footer/footer.jsx';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index/index.css';
+import Homepage from './homepage/Homepage';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="GGLVXD offical website here are a hosted random things" />
-      <meta name="keywords" content="dev, javascript, php, python, minecraft, server, discord, gglvxd, social, about me, linux, terminal, code, ads" />
-      <meta name="author" content="GGLVXD" />
-      <meta name="robots" content="index,follow" />
-    </head>
-    <title>GGLVXD website for uses and my socials</title>
-    <Header />
-    <App />
-    <Footer />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
