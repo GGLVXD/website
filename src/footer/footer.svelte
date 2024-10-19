06a639647fbc7c97cd8ec767f9d6af12a105488a
GGLVXD <gglvmax@gmail.com>
2024-10-19 17:53:17 +0300

    move social media icons below username in homepage requested by @Ronalds13424

--- src/footer/footer.svelte
+++ src/footer/footer.svelte
@@ -1,28 +1,10 @@
 <script>
-    const socialLinks = [
-        { name: 'X', url: 'https://x.com/notgglvxd', icon: 'fa-brands fa-x-twitter' },
-        { name: 'GitHub', url: 'https://github.com/gglvxd', icon: 'fa-brands fa-github' },
-        { name: 'TikTok', url: 'https://tiktok.com/@gglvxd', icon: 'fa-brands fa-tiktok' },
-        { name: 'Instagram', url: 'https://www.instagram.com/gglvxd/', icon: 'fa-brands fa-instagram' },
-        { name: 'YouTube', url: 'https://www.youtube.com/@gglvxd', icon: 'fa-brands fa-youtube' }
-    ];
     import '../footer/footer.css';
 </script>
 
 <footer>
     <div class="footer-content">
         <p>© 2020 GGLVXD.EU.ORG</p>
-        <div class="social-icons">
-            {#each socialLinks as { name, url, icon }}
-                <a href={url} target="_blank" rel="noopener noreferrer" aria-label={name}>
-                    <i class={icon}></i>
-                </a>
-            {/each}
-        </div>
     </div>
 </footer>
-
-<style>
-    @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
-  </style>
 
--- src/homepage/homepage.svelte
+++ src/homepage/homepage.svelte
@@ -1,6 +1,16 @@
 
+<script>
+    const socialLinks = [
+        { name: 'X', url: 'https://x.com/notgglvxd', icon: 'fa-brands fa-x-twitter' },
+        { name: 'GitHub', url: 'https://github.com/gglvxd', icon: 'fa-brands fa-github' },
+        { name: 'TikTok', url: 'https://tiktok.com/@gglvxd', icon: 'fa-brands fa-tiktok' },
+        { name: 'Instagram', url: 'https://www.instagram.com/gglvxd/', icon: 'fa-brands fa-instagram' },
+        { name: 'YouTube', url: 'https://www.youtube.com/@gglvxd', icon: 'fa-brands fa-youtube' }
+    ];
+</script>
 <style>
     @import '../homepage/homepage.css';
+    @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
 </style>
 
 <div class="homepage-container">
@@ -11,6 +21,13 @@
         </div>
         <div class="info">
             <h1 class="name">GGLVXD</h1>
+        </div>
+        <div class="social-icons">
+            {#each socialLinks as { name, url, icon }}
+                <a href={url} target="_blank" rel="noopener noreferrer" aria-label={name}>
+                    <i class={icon}></i>
+                </a>
+            {/each}
         </div>
     </div>
 </div>
